dbc2a712a6ec99c796a65c974fcc4acced409bcb
cristi <cristi@baubaudesign.ro>
2026-06-28 00:39:45 +0300

    Update: Add error warning popup to project creation modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1024,6 +1024,7 @@ export default function App() {
       setActiveView('dashboard');
     } catch (err) {
       console.error('Error creating project:', err);
+      alert('Eroare la crearea proiectului. Detalii: ' + err.message);
     }
   };
 
